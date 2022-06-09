101b360b316e5e702c08d62865f6b0f459c9e746
0xZzzZ <JeanCrypto619@gmail.com>
2022-06-09 15:24:48 +0200

    Update folders

---
+++ backend/graphql/resolvers/merge.js
@@ -0,0 +1,62 @@
+const Event = require("../../../models/event")
+const User =require("../../../models/user")
+const {dateToSstring} = require('../../../helpers/date')
+
+const events = async (eventIds) => {
+  try {
+    const events = await Event.find({ _id: { $in: eventIds } });
+    return events.map((event) => {
+      transformEvent(event);
+    });
+  } catch (err) {
+    throw err;
+  }
+};
+
+const user = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    return {
+      ...user._doc,
+      _id: user.id,
+      createdEvents: events.bind(this, user._doc.createdEvents),
+    };
+  } catch (err) {
+    throw err;
+  }
+};
+const singleEvent = async (eventId) => {
+  try {
+    const event = await Event.findById(eventId);
+    return {
+      ...event._doc,
+      id: event.id,
+      creator: user.bind(this, event.creator),
+    };
+  } catch (err) {
+    throw err;
+  }
+};
+
+const transformEvent = event =>{
+    return {
+        ...event._doc, 
+        _id:event.id,
+        date: dateToSstring(event._doc.date), 
+        creator: user.bind(this,event.creator)
+    };
+}
+const transformBooking = booking => {
+    return{...booking._doc,
+        _id:booking.id,
+        user: user.bind(this, booking._doc.user),
+        event: singleEvent.bind(this, booking._doc.event),
+        createdAt:dateToSstring(booking._doc.createdAt),
+        updatedAt:dateToSstring(booking._doc.updatedAt)}
+}
+
+// exports.user = user;
+// exports.events = events;
+exports.singleEvent = singleEvent;
+exports.transformEvent = transformEvent
+exports.transformBooking = transformBooking
